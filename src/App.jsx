5add079c6937a633899b17b304043519cbcc9168
CurseS1 <Edgartanamal89@gmail.com>
2023-10-13 18:26:38 +0800

    Tugas React element

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-function App() {
-  const [isCircular, setIsCircular] = useState(false);
-
-  const imgStyle = {
-    width: '40%',
-    borderRadius: isCircular ? '50%' : '0%', // Apply border-radius to make it circular
-  };
-
-  const pengalamanStyle = {
-    border: '2px solid #000',
-    padding: '10px',
-  };
-
-  const toggleCircular = () => {
-    setIsCircular(!isCircular);
-  };
-
-  return (
-    <div className="App">
-      <div className="profile-container">
-        <img src="/342220.jpg" className="App-logo" alt="logo" style={imgStyle} />
-      </div>
-
-      <div className="profile-info">
-        <h3 className="name">Edgar Tanamal</h3>
-      </div>
-
-      <div className="gender">
-        <p>Gender</p>
-        <p>goblok</p>
-      </div>
-
-      <div className="userInfo">
-        <div className="info">
-          <h2>Profile</h2>
-        </div>
-      </div>
-
-      <div className="Pengalaman" style={pengalamanStyle}>
-        <h2 className="title">Pengalaman Organisasi</h2>
-        <p>tes</p>
-        
-        {/* Move the button below the "Pengalaman Organisasi" section */}
-        <button onClick={toggleCircular}>Resize Fotto</button>
-      </div>
-    </div>
+function Portofolio() {
+  return React.createElement('div', { className: 'portofolio' },
+    React.createElement('div', { className: 'profil' },
+      React.createElement('img', { src: '/342220.jpg', alt: 'logo' })
+    ),
+    React.createElement('div', { className: 'info' },
+      React.createElement('h2', null, 'Nama: Edgar Tanamal'),
+      React.createElement('div', null,
+        React.createElement('p', null, 'Gender: Pria')
+      ),
+      React.createElement('div', { className: 'pengalaman' },
+        React.createElement('h3', null, 'Pengalaman:'),
+        React.createElement('ul', null,
+          React.createElement('li', null, 'Tidur'),
+          React.createElement('li', null, 'Makan'),
+          React.createElement('li', null, 'Kuliah')
+        )
+      )
+    )
   );
 }
 
-export default App;
+export default Portofolio;
